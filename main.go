e5d6bc61b1c398caeee6276a4a2672ba36909a36
Ingmar Stein <IngmarStein@users.noreply.github.com>
2023-06-28 21:16:51 +0200

    Add support for gen2 devices and beta versions
    Also:
    - Include device address and generation in log messages
    - Update to Go 1.21

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"slices"
 	"strings"
 	"sync"
 	"time"
@@ -16,22 +18,44 @@ import (
 )
 
 const (
-	OtaUrl      = "http://%s/ota"
-	OtaCheckUrl = "http://%s/ota/check"
+	// https://shelly-api-docs.shelly.cloud/gen1/#ota
+	otaUrl = "http://%s/ota"
+
+	// https://shelly-api-docs.shelly.cloud/gen1/#ota-check
+	otaCheckUrl = "http://%s/ota/check"
+
+	// https://shelly-api-docs.shelly.cloud/gen2/ComponentsAndServices/Shelly#shellycheckforupdate
+	checkForUpdateUrl = "http://%s/rpc/Shelly.CheckForUpdate"
+
+	// https://shelly-api-docs.shelly.cloud/gen2/ComponentsAndServices/Shelly#shellyupdate
+	updateUrl = "http://%s/rpc/Shelly.Update?stage=%s"
 )
 
 var (
 	scanTimeout = time.Second * 60
 	username    string
 	password    string
+	updateStage string
+	genToUpdate int
 )
 
 type (
+	versionInfo struct {
+		Version string `json:"version"`
+		BuildId string `json:"build_id"`
+	}
+
+	checkForUpdateResponse struct {
+		Stable versionInfo `json:"stable"`
+		Beta   versionInfo `json:"beta"`
+	}
+
 	shellyUpdateStatusResponse struct {
-		Status     string `json:"status"`
-		HasUpdate  bool   `json:"has_update"`
-		NewVersion string `json:"new_version"`
-		OldVersion string `json:"old_version"`
+		Status      string `json:"status"`
+		HasUpdate   bool   `json:"has_update"`
+		NewVersion  string `json:"new_version"`
+		OldVersion  string `json:"old_version"`
+		BetaVersion string `json:"beta_version"`
 	}
 
 	shellyUpdateCheckResponse struct {
@@ -68,10 +92,13 @@ func makeGetRequest(url string) ([]byte, error) {
 }
 
 func makeShellyUpdateRequest(hostname string, update bool) (*shellyUpdateStatusResponse, error) {
-	url := OtaUrl
-
+	url := otaUrl
 	if update {
-		url += "?update=1"
+		if updateStage == "beta" {
+			url += "?beta=1"
+		} else {
+			url += "?update=1"
+		}
 	}
 
 	body, err := makeGetRequest(fmt.Sprintf(url, hostname))
@@ -89,7 +116,7 @@ func makeShellyUpdateRequest(hostname string, update bool) (*shellyUpdateStatusR
 }
 
 func triggerShellyUpdateCheck(hostname string) (*shellyUpdateCheckResponse, error) {
-	body, err := makeGetRequest(fmt.Sprintf(OtaCheckUrl, hostname))
+	body, err := makeGetRequest(fmt.Sprintf(otaCheckUrl, hostname))
 	if err != nil {
 		return nil, err
 	}
@@ -103,6 +130,30 @@ func triggerShellyUpdateCheck(hostname string) (*shellyUpdateCheckResponse, erro
 	return checkStatus, nil
 }
 
+func makeGen2CheckForUpdateRequest(hostname string) (*checkForUpdateResponse, error) {
+	body, err := makeGetRequest(fmt.Sprintf(checkForUpdateUrl, hostname))
+	if err != nil {
+		return nil, err
+	}
+
+	var checkForUpdate *checkForUpdateResponse
+	err = json.Unmarshal(body, &checkForUpdate)
+	if err != nil {
+		return nil, err
+	}
+
+	return checkForUpdate, nil
+}
+
+func makeGen2UpdateRequest(hostname string, stage string) error {
+	_, err := makeGetRequest(fmt.Sprintf(updateUrl, hostname, stage))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func triggerShellyUpdate(hostname string) (*shellyUpdateStatusResponse, error) {
 	return makeShellyUpdateRequest(hostname, true)
 }
@@ -111,16 +162,13 @@ func checkShellyUpdateStatus(hostname string) (*shellyUpdateStatusResponse, erro
 	return makeShellyUpdateRequest(hostname, false)
 }
 
-func updateShelly(instance *zeroconf.ServiceEntry, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	shellyAddress := instance.AddrIPv4[0].String()
-
+func updateShellyGen1(name, address string) {
+	prefix := fmt.Sprintf("[%s/%s/gen1]", name, address)
 	// First, we trigger a check for updates
-	fmt.Printf("[%s] checking for updates...\n", instance.HostName)
-	_, err := triggerShellyUpdateCheck(shellyAddress)
+	fmt.Printf("%s checking for updates...\n", prefix)
+	_, err := triggerShellyUpdateCheck(address)
 	if err != nil {
-		fmt.Printf("[%s] failed to check for updates: %s, aborting...\n", instance.Instance, err)
+		fmt.Printf("%s failed to check for updates: %s, aborting...\n", prefix, err)
 		return
 	}
 
@@ -128,50 +176,101 @@ func updateShelly(instance *zeroconf.ServiceEntry, wg *sync.WaitGroup) {
 	time.Sleep(time.Second * 5)
 
 	// Then, we check if there are any updates available
-	updateStatus, err := checkShellyUpdateStatus(shellyAddress)
+	updateStatus, err := checkShellyUpdateStatus(address)
 	if err != nil {
-		fmt.Printf("[%s] failed to query update status: %s, aborting...\n", instance.Instance, err)
+		fmt.Printf("%s failed to query update status: %s, aborting...\n", prefix, err)
 		return
 	}
 
 	// If there's an update available, trigger the update
-	if updateStatus.HasUpdate {
+	if (updateStage == "stable" && updateStatus.HasUpdate) ||
+		(updateStage == "beta" && updateStatus.OldVersion != updateStatus.BetaVersion) {
+		newVersion := updateStatus.NewVersion
+		if updateStage == "beta" {
+			newVersion = updateStatus.BetaVersion
+		}
 		fmt.Printf(
-			"[%s] update available! (%s -> %s), updating...\n",
-			instance.HostName, updateStatus.OldVersion,
-			updateStatus.NewVersion,
+			"%s update available! (%s -> %s), updating...\n",
+			prefix, updateStatus.OldVersion, newVersion,
 		)
 
-		updateStatus, err := triggerShellyUpdate(shellyAddress)
+		updateStatus, err := triggerShellyUpdate(address)
 		if err != nil {
-			fmt.Printf("[%s] failed to start update: %s, aborting...\n", instance.Instance, err)
+			fmt.Printf("%s failed to start update: %s, aborting...\n", prefix, err)
 			return
 		}
 
 		for updateStatus.Status == "updating" {
 			time.Sleep(time.Second * 5)
-			updateStatusCheck, err := checkShellyUpdateStatus(shellyAddress)
+			updateStatusCheck, err := checkShellyUpdateStatus(address)
 			if err != nil {
-				fmt.Printf("[%s] failed to query update status: %s, retrying...\n", instance.Instance, err)
+				fmt.Printf("%s failed to query update status: %s, retrying...\n", prefix, err)
 			} else {
 				updateStatus = updateStatusCheck
 			}
 		}
 
-		fmt.Printf("[%s] device updated to %s!\n", instance.Instance, updateStatus.OldVersion)
+		fmt.Printf("%s device updated to %s!\n", prefix, updateStatus.OldVersion)
 	} else {
-		fmt.Printf("[%s] already up to date (%s)\n", instance.Instance, updateStatus.OldVersion)
+		fmt.Printf("%s already up to date (%s)\n", prefix, updateStatus.OldVersion)
+	}
+}
+
+func updateShellyGen2(name, address string) {
+	prefix := fmt.Sprintf("[%s/%s/gen2]", name, address)
+	// First, we trigger a check for updates
+	fmt.Printf("%s checking for updates...\n", prefix)
+	updates, err := makeGen2CheckForUpdateRequest(address)
+	if err != nil {
+		fmt.Printf("%s failed to check for updates: %s, aborting...\n", prefix, err)
+		return
+	}
+
+	updateVersion := updates.Stable.Version
+	if updateStage == "beta" {
+		updateVersion = updates.Beta.Version
+	}
+	if updateVersion == "" {
+		fmt.Printf("%s already up to date\n", prefix)
+		return
+	}
+
+	fmt.Printf("%s updating to version %s...\n", prefix, updateVersion)
+	err = makeGen2UpdateRequest(address, updateStage)
+	if err != nil {
+		fmt.Printf("%s failed to update: %s, aborting...\n", prefix, err)
+		return
+	}
+}
+
+func updateShelly(name, address string, txtRecords []string, genToUpdate int) {
+	if slices.Contains(txtRecords, "gen=2") {
+		if genToUpdate == 2 || genToUpdate == 0 {
+			updateShellyGen2(name, address)
+		}
+		return
+	}
+
+	if genToUpdate == 1 || genToUpdate == 0 {
+		updateShellyGen1(name, address)
 	}
 }
 
 func main() {
 	flag.StringVar(&username, "username", "admin", "username to use for authentication")
 	flag.StringVar(&password, "password", "", "password to use for authentication")
+	flag.StringVar(&updateStage, "stage", "stable", "stable or beta")
+	flag.IntVar(&genToUpdate, "gen", 0, "device generation to update (default: all)")
 
 	flag.Parse()
 
 	if password != "" {
 		fmt.Printf("Using basic authentication: %s:*******\n", username)
+	}
+
+	if updateStage != "stable" && updateStage != "beta" {
+		flag.Usage()
+		os.Exit(2)
 	}
 
 	resolver, err := zeroconf.NewResolver(nil)
@@ -184,9 +283,20 @@ func main() {
 	go func(results <-chan *zeroconf.ServiceEntry) {
 		fmt.Printf("[scanner] looking for Shelly devices using mDNS (%ds timeout)...\n", int(scanTimeout.Seconds()))
 		for entry := range results {
-			if strings.HasPrefix(entry.Instance, "shelly") {
+			entry := entry
+			if strings.HasPrefix(strings.ToLower(entry.Instance), "shelly") {
 				wg.Add(1)
-				go updateShelly(entry, &wg)
+				go func() {
+					address := entry.HostName
+					if len(entry.AddrIPv4) > 0 {
+						address = entry.AddrIPv4[0].String()
+						// not yet:
+						//} else if len(entry.AddrIPv6) > 0 {
+						//	address = fmt.Sprintf("[%s]", entry.AddrIPv6[0].String())
+					}
+					updateShelly(entry.Instance, address, entry.Text, genToUpdate)
+					wg.Done()
+				}()
 			}
 		}
 		fmt.Println("[scanner] scanning process finished")
